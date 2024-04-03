9dc146ddf1a0ff782bb9f31d476c35268e8727c8
kskyoshiki <70670791+kskyoshiki@users.noreply.github.com>
2024-04-03 16:48:55 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { Button, ChakraProvider } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <button>ボタン</button>
+    </ChakraProvider>
+  );
+}
+
+export default App;
